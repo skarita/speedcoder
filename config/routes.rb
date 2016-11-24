b506c7a978fb42a2bfc2990100e5aee6c46c5ddd
James Mah <junyeumah@gmail.com>
2016-11-24 16:31:46 +1100

    minor refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
-  resources :snippets
-  resources :attempts
+  resources :users, :snippets, :attempts
 
   get '/session/new', to: 'session#new'
   post '/session', to: 'session#create'
